d5a2d470887d2761e2bd807eef44cb27d304bdc8
Chloe <196950068+RubberSinking@users.noreply.github.com>
2026-04-02 18:55:45 +0000

    daily-song: revert no-cache headers

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
-header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
-header('Pragma: no-cache');
-header('Expires: 0');
-
 if (!empty($_GET['lyrics'])) {
     $slug = basename($_GET['lyrics']);
     $path = __DIR__ . '/lyrics/' . $slug;
